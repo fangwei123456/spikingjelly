49eaf5b92cb7754a2b1dc59fbc7e395628da079f
V6pro <fangwei123456@pku.edu.cn>
2020-01-18 19:32:28 +0800

    文档测试

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,3 +62,5 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ['torch', 'numpy']
