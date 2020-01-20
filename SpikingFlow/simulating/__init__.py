fba0180cc3cf7db72fa87ac408cefbc21556aef2
V6pro <fangwei123456@pku.edu.cn>
2020-01-20 22:18:18 +0800

    补全simulating文档

--- SpikingFlow/simulating/__init__.py
+++ SpikingFlow/simulating/__init__.py
@@ -3,70 +3,73 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Simulator:
-    '''
-    仿真器，内含多个module组成的list
-
-    测试代码如下
-    sim = simulating.Simulator()
-    sim.append(encoding.ConstantEncoder())
-    sim.append(tf.SpikeCurrent(amplitude=0.01))
-    sim.append(neuron.IFNode(shape=[1], r=0.5, v_threshold=1.0))
-    sim.append(tf.SpikeCurrent(amplitude=0.4))
-    sim.append(neuron.IFNode(shape=[1], r=2.0, v_threshold=1.0))
-    sim.append(tf.ExpDecayCurrent(tau=5.0, amplitude=1.0))
-    sim.append(neuron.LIFNode(shape=[1], r=5.0, v_threshold=1.0, tau=10.0))
-    v = []
-    v.extend(([], [], []))
-    for i in range(1000):
-        if i < 800:
-            output_data = sim.step(torch.ones(size=[1], dtype=torch.bool))
-        else:
-            output_data = sim.step(torch.zeros(size=[1], dtype=torch.bool))
-
-
-        #print(i, sim.pipeline)
-        for j in range(3):
-            v[j].append(sim.module_list[2 * j + 2].v.item())
-
-    pyplot.plot(v[0])
-    pyplot.show()
-    pyplot.plot(v[1])
-    pyplot.show()
-    pyplot.plot(v[2])
-    pyplot.show()
-    '''
     def __init__(self):
+        '''
+        仿真器，内含多个module组成的list
+
+        当前时刻启动仿真前，数据和模型如下
+
+        x[0] -> module[0] -> x[1] -> module[1] -> ... -> x[n-1] -> module[n-1] -> x[n]
+
+        pipeline = [ x[0], x[1], ..., x[n-2], x[n-1], x[n] ]
+
+        启动仿真后 应该按照如下顺序计算
+
+        x[n] = module[n-1](x[n-1])
+
+        x[n-1] = module[n-2](x[n-2])
+
+        ...
+
+        x[1] = module[0](x[0])
+
+
+        测试代码
+
+        .. code-block:: python
+            sim = simulating.Simulator()
+            sim.append(encoding.ConstantEncoder())
+            sim.append(tf.SpikeCurrent(amplitude=0.01))
+            sim.append(neuron.IFNode(shape=[1], r=0.5, v_threshold=1.0))
+            sim.append(tf.SpikeCurrent(amplitude=0.4))
+            sim.append(neuron.IFNode(shape=[1], r=2.0, v_threshold=1.0))
+            sim.append(tf.ExpDecayCurrent(tau=5.0, amplitude=1.0))
+            sim.append(neuron.LIFNode(shape=[1], r=5.0, v_threshold=1.0, tau=10.0))
+            v = []
+            v.extend(([], [], []))
+            for i in range(1000):
+                if i < 800:
+                    output_data = sim.step(torch.ones(size=[1], dtype=torch.bool))
+                else:
+                    output_data = sim.step(torch.zeros(size=[1], dtype=torch.bool))
+
+
+                #print(i, sim.pipeline)
+                for j in range(3):
+                    v[j].append(sim.module_list[2 * j + 2].v.item())
+
+            pyplot.plot(v[0])
+            pyplot.show()
+            pyplot.plot(v[1])
+            pyplot.show()
+            pyplot.plot(v[2])
+            pyplot.show()
+        '''
         self.module_list = []  # 保存各个module
         self.pipeline = []  # 保存各个module在当前时刻的输出
         self.simulated_steps = 0  # 已经运行仿真的步数
         self.pipeline.append(None)
 
-        '''        
-        当前时刻启动仿真前 数据和模型如下
-        x[0] -> module[0] -> x[1] -> module[1] -> ... -> x[n-1] -> module[n-1] -> x[n]
-        pipeline = [ x[0], x[1], ..., x[n-2], x[n-1], x[n] ]
-        启动仿真后 应该按照如下顺序计算
-        x[n] = module[n-1](x[n-1])
-        x[n-1] = module[n-2](x[n-2])
-        ...
-        x[1] = module[0](x[0])
-        
-        需要注意的是，只有运行n = module_list.__len__()步仿真后，才能得到第一个输出x[n]，此时pipeline才完全充满
-        运行了simulated_steps步时，只得到了x[0], x[1], ..., x[simulated_steps]
-        因此，当simulated_steps < module_list.__len__()时，每进行一步仿真，实际上只能进行如下运算
-        
-        x[simulated_steps+1] = module[simulated_steps](x[simulated_steps])
-        x[simulated_steps] = module[simulated_steps-1](x[simulated_steps-1])
-        ...
-        x[1] = module[0](x[0])
-        '''
 
 
     def append(self, new_module):
         '''
-        向Simulator中添加神经元、突触等模型
         :param new_module: 新添加的模型
-        :return: 无返回值
+        :return: None
+
+        向Simulator的module_list中添加module
+
+        只要是torch.nn.Module的子类并定义了forward()方法，就可以添加=
         '''
         self.module_list.append(new_module)
         self.pipeline.append(None)
@@ -75,9 +78,11 @@ class Simulator:
 
     def step(self, input_data):
         '''
-        输入数据input_data，仿真一步
         :param input_data: 输入数据
         :return: 输出值
+
+        输入数据input_data，仿真一步
+
         '''
         self.pipeline[0] = input_data
 
@@ -92,12 +97,10 @@ class Simulator:
 
         else:
             for i in range(self.module_list.__len__(), 0, -1):
-                '''
-                x[n] = module[n-1](x[n-1])
-                x[n-1] = module[n-2](x[n-2])
-                ...
-                x[1] = module[0](x[0])
-                '''
+                #  x[n] = module[n-1](x[n-1])
+                #  x[n-1] = module[n-2](x[n-2])
+                #  ...
+                #  x[1] = module[0](x[0])
                 self.pipeline[i] = self.module_list[i - 1](self.pipeline[i - 1])
 
         self.simulated_steps += 1
@@ -105,6 +108,8 @@ class Simulator:
 
     def reset(self):
         '''
+        :return: None
+
         重置仿真器到开始仿真前的状态，已经添加的module并不会被清除
         '''
         self.simulated_steps = 0
