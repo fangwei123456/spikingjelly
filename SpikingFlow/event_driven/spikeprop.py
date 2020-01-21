7e93ea41ec5aa19db2179ab0fa94721b1a3ec8d1
fangwei123456 <fangwei123456@pku.edu.cn>
2020-01-21 13:33:23 +0800

    补全event_driven文档

--- SpikingFlow/event_driven/spikeprop.py
+++ SpikingFlow/event_driven/spikeprop.py
@@ -17,6 +17,15 @@ class SpikePropLayer(nn.Module):
     '''
 
     def __init__(self, in_num, out_num, T, v_threshold, kernel_function, grad_kernel_function, *args):
+        '''
+        :param in_num: 输入数量
+        :param out_num: 输出数量
+        :param T: 仿真时长
+        :param v_threshold: 阈值
+        :param kernel_function:  :math:`v(t)` 函数
+        :param grad_kernel_function:  :math:`v'(t)` 函数，即为 :math:`v(t)` 函数的导数，反向传播时要用到
+        :param args: :math:`v(t)` 函数的输入参数
+        '''
         super(SpikePropLayer, self).__init__()
         self.in_num = in_num
         self.out_num = out_num
@@ -30,27 +39,24 @@ class SpikePropLayer(nn.Module):
 
     def forward(self, in_spike):
         '''
-        :param in_spike:输入[batch_size, in_num]，是in_num个神经元的脉冲发放时间
-
-        :return:
+        :param in_spike: [batch_size, in_num]，是in_num个神经元的脉冲发放时间
+        :return:  [batch_size, out_num]，是out_num个神经元的脉冲发放时间
         '''
         device = in_spike.device
         if self.t_sequence.device != device:
             self.t_sequence = self.t_sequence.to(device)
-        '''
-                create time_sequence with shape=[batch_size, in_num, T]
-                t_sequence = [
-                [0, 1, 2, ..., T-1],
-                [0, 1, 2, ..., T-1],
-                ...................,
-                [0, 1, 2, ..., T-1]
-                ]
-                time_sequence[?][i] = [
-                0 - t_spike[?][i], 1 - t_spike[?][i], ..., T - 1 - t_spike[?][i]
-                ]
 
+        # create time_sequence with shape=[batch_size, in_num, T]
+        # t_sequence = [
+        # [0, 1, 2, ..., T-1],
+        # [0, 1, 2, ..., T-1],
+        # ...................,
+        # [0, 1, 2, ..., T-1]
+        # ]
+        # time_sequence[?][i] = [
+        # 0 - t_spike[?][i], 1 - t_spike[?][i], ..., T - 1 - t_spike[?][i]
+        # ]
 
-        '''
         time_sequence = (self.t_sequence - in_spike.unsqueeze(2)).float()  # [batch_size, in_num, T]
 
         v_input = self.kernel_function(time_sequence, *self.args)  # [batch_size, in_num, T]
@@ -63,15 +69,14 @@ class SpikePropLayer(nn.Module):
 class VoltageToSpike(torch.autograd.Function):
     '''
     输入膜电位电压，输出脉冲发放时间
+
     脉冲定义为膜电位电压首次到达阈值的时刻，只允许一次脉冲发放
+
+    使用线性化假设来求导
     '''
 
     @staticmethod
     def forward(ctx, membrane_voltage, weight, time_sequence, v_threshold, kernel_function, grad_kernel_function, *args):
-        '''
-        输入膜电位membrane_voltage shape=[batch_size, out_num, T]
-        输出脉冲发放时刻out_spike shape=[batch_size, out_num]
-        '''
         batch_size = membrane_voltage.shape[0]
         in_num = weight.shape[1]
         out_num = weight.shape[0]
@@ -113,56 +118,49 @@ class VoltageToSpike(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        '''
-        :param ctx:
-        :param grad_output:
-        :return:
 
-        grad_output.shape=[batch_size, out_num], same with out_spike
+        # grad_output.shape=[batch_size, out_num], same with out_spike
+        # 使用以下符号
+        # 输入脉冲S1, [batch_size, in_num]
+        # 经过核函数作用，得到电压X1, [batch_size, in_num, T]
+        # 经过权重weight=W1, [out_num, in_num], 作用，得到电压Y1, [batch_size, out_num, T]
+        # 由电压Y1产生脉冲S2, [batch_size, out_num]
 
-        使用以下符号
-        输入脉冲S1, [batch_size, in_num]
-        经过核函数作用，得到电压X1, [batch_size, in_num, T]
-        经过权重weight=W1, [out_num, in_num], 作用，得到电压Y1, [batch_size, out_num, T]
-        由电压Y1产生脉冲S2, [batch_size, out_num]
-
-        '''
         out_spike, dX1_dt, weight, shape_tensor = ctx.saved_tensors
 
         batch_size, out_num = grad_output.shape
         in_num = weight.shape[1]
         args_len = shape_tensor[0].item()
         T = shape_tensor[1].item()
-        '''
-        使用以下符号
-        输入脉冲S1, [batch_size, in_num]
-        经过核函数作用，得到电压X1, [batch_size, in_num, T]
-        经过权重weight=W1, [out_num, in_num], 作用，得到电压Y1, [batch_size, out_num, T]
-        由电压Y1产生脉冲S2, [batch_size, out_num]
-        首先求解Y1, [batch_size, out_num, T]在脉冲发放时刻对时间的导数，尺寸应该为[batch_size, out_num]
-        d(Y1) / dt = d(X1.matmul(W1.t())) / dt = (d(X1) / dt).matmul(W1.t())
-        由于out_num个输出神经元的脉冲发放时间并不相同，因此输出层神经元i在脉冲发放时刻t_i的电压Y1[:, i, t_i]对时间的导数
-        应该为X1[:, :, t_i]对时间的导数，再矩阵乘W1[i].t()
-        X1[:, :, t_i]对时间的导数为t_i时刻核函数对时间的导数
 
-        dX1_dt shape=[batch_size, in_num, out_num]
-        dX1_dt[:, :, i]是out_spike[:, i]时刻输入层神经元电压对时间的导数
-        '''
+        # 使用以下符号
+        # 输入脉冲S1, [batch_size, in_num]
+        # 经过核函数作用，得到电压X1, [batch_size, in_num, T]
+        # 经过权重weight=W1, [out_num, in_num], 作用，得到电压Y1, [batch_size, out_num, T]
+        # 由电压Y1产生脉冲S2, [batch_size, out_num]
+        # 首先求解Y1, [batch_size, out_num, T]在脉冲发放时刻对时间的导数，尺寸应该为[batch_size, out_num]
+        # d(Y1) / dt = d(X1.matmul(W1.t())) / dt = (d(X1) / dt).matmul(W1.t())
+        # 由于out_num个输出神经元的脉冲发放时间并不相同，因此输出层神经元i在脉冲发放时刻t_i的电压Y1[:, i, t_i]对时间的导数
+        # 应该为X1[:, :, t_i]对时间的导数，再矩阵乘W1[i].t()
+        # X1[:, :, t_i]对时间的导数为t_i时刻核函数对时间的导数
+        #
+        # dX1_dt shape=[batch_size, in_num, out_num]
+        # dX1_dt[:, :, i]是out_spike[:, i]时刻输入层神经元电压对时间的导数
+
         dY1_dt = torch.zeros_like(out_spike)
 
         for i in range(out_num):
-            '''
-            需要注意，不发放脉冲的情况，脉冲发放时间被设置成T，在kernel_cuntion中会得到0
-            而在grad_kernel_cuntion中也会计算得到0
-            '''
+
+            # 需要注意，不发放脉冲的情况，脉冲发放时间被设置成T，在kernel_cuntion中会得到0
+            # 而在grad_kernel_cuntion中也会计算得到0
             dY1_dt[:, i] = dX1_dt[:, :, i].matmul(weight[i].t())
 
-        '''
-        接下来计算d(S2) / d(Y1)，它应该就是-1 / dY1_dt
-        通过乘(dY1_dt != 0).float()，防止-1/0出现nan的情况
-        grad_output shape=[batch_size, out_num]
-        grad_output = d(Loss) / d(S2)
-        '''
+
+        # 接下来计算d(S2) / d(Y1)，它应该就是-1 / dY1_dt
+        # 通过乘(dY1_dt != 0).float()，防止-1/0出现nan的情况
+        # grad_output shape=[batch_size, out_num]
+        # grad_output = d(Loss) / d(S2)
+
         dS2_dY1 = -1 / dY1_dt * ((dY1_dt != 0).float())  # [batch_size, out_num]
         dL_dY1 = dS2_dY1 * grad_output  # [batch_size, out_num]
         grad_membrane_voltage = torch.zeros(size=[batch_size, out_num, T], device=grad_output.device)
