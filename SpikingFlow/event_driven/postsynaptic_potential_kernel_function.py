7a9d3c082d7600c0f967972eb0e787e3568b3aa9
fangwei123456 <fangwei123456@pku.edu.cn>
2020-01-21 10:54:17 +0800

    补全event_driven文档

--- SpikingFlow/event_driven/postsynaptic_potential_kernel_function.py
+++ SpikingFlow/event_driven/postsynaptic_potential_kernel_function.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-
+'''
+这个py文件包含各种 :math:`v(t)` 函数
+'''
 def exp_decay_kernel(t, tau=15.0, tau_s=15.0 / 4):
     t_ = F.relu(t)
     return torch.exp(-t_ / tau) - torch.exp(-t_ / tau_s)
