1701d2a61d739701f2169e93bc308ca95cb01588
fangwei123456 <fangwei123456@pku.edu.cn>
2020-02-17 20:20:04 +0800

    补充“快速上手教程”文档

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ['torch', 'numpy', 'SpikingFlow']
+autodoc_mock_imports = ['torch', 'numpy']
 autoclass_content = 'both'
 autodoc_member_order = 'bysource'
 
