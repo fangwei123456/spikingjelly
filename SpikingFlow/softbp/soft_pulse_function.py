8202bb4e65633df77e8803c65890cf712369f2a7
fangwei123456 <fangwei123456@pku.edu.cn>
2020-06-29 19:30:31 +0800

    修改spiking()函数的实现；增加monitor，可以记录神经元的电压和脉冲

--- SpikingFlow/softbp/soft_pulse_function.py
+++ SpikingFlow/softbp/soft_pulse_function.py
@@ -11,7 +11,7 @@ class bilinear_leaky_relu(torch.autograd.Function):
 
         ctx.save_for_backward(piecewise0 * b + piecewise1 * a + piecewise2 * b)
 
-        return (b * x + b * c - a * c) * piecewise0 + (a * x) * piecewise1 + (b * x - b * c + a * c) * piecewise2
+        return (x >= 0).float()
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -29,7 +29,16 @@ class BilinearLeakyReLU(nn.Module):
         :param c: 决定梯度区间的参数
         :return: 与输入相同shape的输出
 
-        双线性的近似脉冲发放函数。前向为
+        双线性的近似脉冲发放函数。梯度为
+
+        .. math::
+            g'(x) =
+            \\begin{cases}
+            a, & -c \\leq x \\leq c \\\\
+            b, & x < -c ~or~ x > c
+            \\end{cases}
+
+        对应的原函数为
 
         .. math::
             g(x) =
@@ -37,15 +46,6 @@ class BilinearLeakyReLU(nn.Module):
             bx + bc - ac, & x < -c \\\\
             ax, & -c \\leq x \\leq c \\\\
             bx - bc + ac, & x > c \\\\
-            \\end{cases}
-
-        反向为
-
-        .. math::
-            g'(x) =
-            \\begin{cases}
-            a, & -c \\leq x \\leq c \\\\
-            b, & x < -c ~or~ x > c
             \\end{cases}
 
         '''
@@ -57,6 +57,25 @@ class BilinearLeakyReLU(nn.Module):
     def forward(self, x):
         return self.f(x, self.a, self.b, self.c)
 
+class sigmoid(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, alpha):
+        alpha_x = x * alpha
+        ctx.save_for_backward(alpha_x)
+        ctx.alpha = alpha
+
+        return (alpha_x >= 0).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_x = None
+        if ctx.needs_input_grad[0]:
+            alpha_x = ctx.saved_tensors[0]
+            s_x = torch.sigmoid(alpha_x)
+            grad_x = grad_output * s_x * (1 - s_x) * ctx.alpha
+
+        return grad_x, None
+
 class Sigmoid(nn.Module):
     def __init__(self, alpha=1.0):
         '''
@@ -64,30 +83,29 @@ class Sigmoid(nn.Module):
         :param alpha: 控制反向传播时梯度的平滑程度的参数
         :return: 与输入相同shape的输出
 
-        反向传播时使用sigmoid的梯度的脉冲发放函数。前向为
-
-        .. math::
-            g(x) = \\mathrm{sigmoid}(\\alpha x) = \\frac{1}{1+e^{-\\alpha x}}
-
-        反向为
+        反向传播时使用sigmoid的梯度的脉冲发放函数。反向传播为
 
         .. math::
             g'(x) = \\alpha * (1 - \\mathrm{sigmoid} (\\alpha x)) \\mathrm{sigmoid} (\\alpha x)
+
+        对应的原函数为
+
+        .. math::
+            g(x) = \\mathrm{sigmoid}(\\alpha x) = \\frac{1}{1+e^{-\\alpha x}}
         '''
         super().__init__()
         self.alpha = alpha
+        self.f = sigmoid.apply
     def forward(self, x):
-        return torch.sigmoid(self.alpha * x)
+        return self.f(x, self.alpha)
 
 class sign_swish(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, beta=1.0):
         beta_x = beta * x
-        s_beta_x = torch.sigmoid(beta_x)
-
         ctx.save_for_backward(beta_x)
         ctx.beta = beta
-        return 2 * s_beta_x * (1 + beta_x * (1 - s_beta_x)) - 1
+        return (x >= 0).float()
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -108,15 +126,15 @@ class SignSwish(nn.Module):
 
         Darabi, Sajad, et al. "BNN+: Improved binary network training." arXiv preprint arXiv:1812.11800 (2018).
 
-        反向传播时使用swish的梯度的脉冲发放函数。前向为
-
-        .. math::
-            g(x) = 2 * \\mathrm{sigmoid}(\\beta x) * (1 + \\beta x (1 - \\mathrm{sigmoid}(\\beta x))) - 1
-
-        反向为
+        反向传播时使用swish的梯度的脉冲发放函数。反向传播为
 
         .. math::
             g'(x) = \\frac{\\beta (2 - \\beta x \\mathrm{tanh} \\frac{\\beta x}{2})}{1 + \\mathrm{cosh}(\\beta x)}
+
+        对应的原函数为
+
+        .. math::
+            g(x) = 2 * \\mathrm{sigmoid}(\\beta x) * (1 + \\beta x (1 - \\mathrm{sigmoid}(\\beta x))) - 1
         '''
         super().__init__()
         self.beta = beta
