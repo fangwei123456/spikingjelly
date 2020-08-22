11e2303a33cd1df9cdf347fd68a889fcb6326436
Yanqi-Chen <chyq@pku.edu.cn>
2020-08-22 15:49:31 +0800

    python版本指定

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("./README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     install_requires=install_requires,
     name="spikingflow",
-    version="0.2.4.5.938",
+    version="0.2.4.5.939",
     author="PKU MLG and other contributors",
     author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
     description="A Spiking Neural Networks simulator built on PyTorch.",
@@ -22,7 +22,10 @@ setuptools.setup(
     url="https://github.com/fangwei123456/spikingflow",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
