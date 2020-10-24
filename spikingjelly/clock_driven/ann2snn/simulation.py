e2540ce3603a1d72bddfacef42b04c7b9b4faef8
fangwei <fangwei@k660e>
2020-10-24 16:39:15 +0800

    增加多GPU并行仿真

--- spikingjelly/clock_driven/ann2snn/simulation.py
+++ spikingjelly/clock_driven/ann2snn/simulation.py
@@ -7,7 +7,6 @@ import spikingjelly.clock_driven.functional as functional
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
-
 def simulate_snn(snn, device, data_loader, T, poisson=False, online_draw=False,fig_name='default',ann_baseline=0,save_acc_list=False,log_dir=None): # TODO ugly
     '''
     * :ref:`API in English <simulate_snn-en>`
@@ -99,3 +98,96 @@ def simulate_snn(snn, device, data_loader, T, poisson=False, online_draw=False,f
         plt.ioff()
         plt.close()
     return acc
+
+import copy
+import torch.utils.data
+import threading
+class FunctionThreadWithRet(threading.Thread):
+    def __init__(self, f, *args, **kwargs):
+        super().__init__()
+        self.ret = None
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.ret = self.f(*self.args, **self.kwargs)
+
+    def get_ret(self):
+        return self.ret
+
+def parallel_inference_correct_sum_loss(net: nn.Module, data_loader: torch.utils.data.DataLoader, device: str):
+    '''
+    :param net: 用于分类任务的网络，输出是一个 ``shape = [batch_size, C]`` 的tensor
+    :type net: nn.Module
+    :param data_loader: 分类任务数据（例如 ``torchvision.datasets.MNIST``）的加载器
+    :type data_loader: torch.utils.data.DataLoader
+    :param device: 网络所在的设备
+    :type device: str
+    :return: 分类正确的总数
+    :rtype: int
+    '''
+    correct_sum = 0
+    net.eval()
+    with torch.no_grad():
+        for x, y in data_loader:
+            y_p = net(x.to(device))
+            correct_sum += (y_p.argmax(dim=1) == y.to(device)).sum().item()
+    return correct_sum
+
+def parallel_inference(net: nn.Module, data_loaders: list, devices: list, loss_fun, *args, **kwargs):
+    '''
+    使用示例：
+    .. code-block:: python
+
+        class TestNet(nn.Module):
+            def __init__(self, class_num):
+                super().__init__()
+                self.class_num = class_num
+            def forward(self, x: torch.Tensor):
+                return torch.rand(size=[x.shape[0], self.class_num]).to(x)
+        class TestClassifyDataset(torch.utils.data.Dataset):
+            def __init__(self, class_num, data_num):
+                self.class_num = class_num
+                self.data_num = data_num
+                self.x = torch.rand(size=[self.data_num])
+                self.y = torch.randint(0, self.class_num, size=[self.data_num])
+            def __len__(self):
+                return self.data_num
+            def __getitem__(self, index):
+                return self.x[index], self.y[index]
+
+        class_num = 10
+        origin_dataset = TestClassifyDataset(class_num=class_num, data_num=10000)
+        data_num = origin_dataset.__len__()
+        devices = ['cuda:9', 'cuda:10', 'cuda:11', 'cuda:13']
+        block_num = data_num // devices.__len__()
+        data_loaders = []
+        for i in range(devices.__len__()):
+            data_loaders.append(
+                torch.utils.data.DataLoader(
+                    dataset=torch.utils.data.Subset(origin_dataset, list(range(i * block_num, min(data_num, (i + 1) * block_num)))),
+                    batch_size=128
+                )
+            )
+
+
+        correct_sum = parallel_inference(TestNet(class_num=class_num), data_loaders, devices, parallel_inference_correct_sum_loss)
+        print(f'accuracy={correct_sum / data_num}')
+    '''
+    inf_threads = []
+    for i in range(devices.__len__()):
+        net_c = net.to(devices[i])
+        inf_threads.append(FunctionThreadWithRet(loss_fun, net_c, data_loaders[i], devices[i], *args, **kwargs))
+        print(f'start thread {i} on device {devices[i]}')
+        inf_threads[-1].start()
+
+    loss_sum = 0
+    for i in range(devices.__len__()):
+        inf_threads[i].join()
+        print(f'finish thread {i} on device {devices[i]}')
+        loss_sum += inf_threads[i].get_ret()
+    return loss_sum
+
+
+
