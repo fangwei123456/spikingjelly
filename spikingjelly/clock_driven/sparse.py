e2e7dc7274e35acb49944990b0c2929a6cd0fe9a
fangwei-V6pro <708922276@qq.com>
2020-10-31 15:27:09 +0800

    fix bug

--- spikingjelly/clock_driven/sparse.py
+++ spikingjelly/clock_driven/sparse.py
@@ -4,6 +4,9 @@ import torch.nn.functional as F
 import MinkowskiEngine as ME
 
 def to_sparse(x: torch.Tensor):
+    # x.shape=[B, d1, d2, ..., dn]
+    # 返回的coords.shape=[M, n], feats.shape=[M, 1]
+    # M 是非0元素的数量
     '''
     .. code-block:: python
 
@@ -13,14 +16,14 @@ def to_sparse(x: torch.Tensor):
         coords, feats = to_sparse(spike)
         x = ME.SparseTensor(coords=coords, feats=feats)
         print(x)
-        print(x.dense())
+        print(x.dense(min_coords=torch.IntTensor([0, 0]), max_coords=torch.IntTensor([2, 1])))
     '''
-    assert x.dim() >= 2, 'x must be a tensor with more than 1 dim'
+    assert x.dim() >= 3, 'must ensure x.dim() >= 3'
 
     if x.dtype == torch.bool:
         mask = x
     else:
-        mask = x > 0
+        mask = x != 0
     coords = []
 
     for i in range(x.dim()):
@@ -43,55 +46,10 @@ def to_sparse(x: torch.Tensor):
         feats = x[mask].unsqueeze_(1)
     return coords, feats
 
-class SparseConverter:
-    @ staticmethod
-    def str_tensor_shape(x: torch.Tensor):
-        # 将x的尺寸转换为字符串，例如'2-3'
-        shape_list = list(x.shape)
-        for i in range(shape_list.__len__()):
-            shape_list[i] = str(shape_list[i])
-        return '-'.join(shape_list)
-
-    def __init__(self):
-        self.index_buffer = {}  # 保存生成索引的tensor的缓冲区
-
-    def to_sparse(self, x: torch.Tensor):
-        assert x.dim() >= 2, 'x must be a tensor with more than 1 dim'
-        if x.dtype == torch.bool:
-            mask = x
-        else:
-            mask = x > 0
-
-        str_shape = SparseConverter.str_tensor_shape(x)
-
-        coords = []
-        for i in range(x.dim()):
-            # 生成脉冲数据的第i维的索引
-            str_shape_i = str_shape + '_' + str(i)
-
-            if str_shape_i not in self.index_buffer:
-                index_shape = [1] * x.dim()
-                index_shape[i] = x.shape[i]
-
-                index_tensor = torch.arange(0, x.shape[i], device=x.device).view(index_shape)
-
-                repeat_shape = list(x.shape)
-                repeat_shape[i] = 1
-                self.index_buffer[str_shape_i] = index_tensor.repeat(repeat_shape)
-
-            coords.append(self.index_buffer[str_shape_i][mask].unsqueeze_(0))
-
-            # 若x为 tensor([[ True, False,  True],
-            #                  [ True, False, False]])
-            # 运行到此处，coords为 [tensor([[0, 0, 1]]), tensor([[0, 2, 0]])]
-
-        coords = torch.cat(coords).t().int()
-        coords = torch.cat(coords).t().int()
-        if x.dtype == torch.bool:
-            feats = torch.ones(size=[coords.shape[0], 1], dtype=torch.float, device=x.device)
-        else:
-            feats = x[mask].unsqueeze_(1)
-        return coords, feats
+class ToSparse(nn.Module):
+    def forward(self, x: torch.Tensor):
+        coords, feats = to_sparse(x)
+        return ME.SparseTensor(coords=coords, feats=feats)
 
 class SparseMaxPool3d(ME.MinkowskiMaxPooling):
     def __init__(self, kernel_size, stride=1, dilation=1):
@@ -105,3 +63,28 @@ class SparseMaxPool1d(ME.MinkowskiMaxPooling):
     def __init__(self, kernel_size, stride=1, dilation=1):
         super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=2)
 
+
+class SparseSumPool3d(ME.MinkowskiSumPooling):
+    def __init__(self, kernel_size, stride=1, dilation=1):
+        super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=4)
+
+class SparseSumPool2d(ME.MinkowskiSumPooling):
+    def __init__(self, kernel_size, stride=1, dilation=1):
+        super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=3)
+
+class SparseSumPool1d(ME.MinkowskiSumPooling):
+    def __init__(self, kernel_size, stride=1, dilation=1):
+        super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=2)
+
+
+class SparseConv3d(ME.MinkowskiConvolution):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True):
+        super().__init__(self, in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, dilation=dilation, has_bias=bias, dimension=4)
+
+class SparseConv2d(ME.MinkowskiConvolution):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True):
+        super().__init__(self, in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, dilation=dilation, has_bias=bias, dimension=3)
+
+class SparseConv1d(ME.MinkowskiConvolution):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, bias=True):
+        super().__init__(self, in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, dilation=dilation, has_bias=bias, dimension=2)
