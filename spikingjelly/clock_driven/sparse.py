c20705dc8756a8b65829cf56fc889d071afb357e
fangwei-V6pro <708922276@qq.com>
2020-10-31 10:06:03 +0800

    fix bug

--- spikingjelly/clock_driven/sparse.py
+++ spikingjelly/clock_driven/sparse.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import MinkowskiEngine as ME
 
-def to_sparse(spike: torch.BoolTensor):
+def to_sparse(x: torch.Tensor):
     '''
     .. code-block:: python
 
@@ -15,26 +15,29 @@ def to_sparse(spike: torch.BoolTensor):
         print(x)
         print(x.dense())
     '''
-    assert spike.dim() >= 2, 'spike must be a tensor with more than 1 dim'
+    assert x.dim() >= 2, 'x must be a tensor with more than 1 dim'
 
+    if x.dtype == torch.bool:
+        mask = x
+    else:
+        mask = x > 0
     coords = []
 
-    for i in range(spike.dim()):
-        index_shape = [1] * spike.dim()
-        index_shape[i] = spike.shape[i]
+    for i in range(x.dim()):
+        index_shape = [1] * x.dim()
+        index_shape[i] = x.shape[i]
 
-        index_tensor = torch.arange(0, spike.shape[i], device=spike.device).view(index_shape)
+        index_tensor = torch.arange(0, x.shape[i], device=x.device).view(index_shape)
 
-        repeat_shape = list(spike.shape)
+        repeat_shape = list(x.shape)
         repeat_shape[i] = 1
-        coords.append(index_tensor.repeat(repeat_shape)[spike].unsqueeze_(0))
-    # 若spike为 tensor([[ True, False,  True],
+        coords.append(index_tensor.repeat(repeat_shape)[mask].unsqueeze_(0))
+    # 若x为 tensor([[ True, False,  True],
     #                  [ True, False, False]])
     # 运行到此处，coords为 [tensor([[0, 0, 1]]), tensor([[0, 2, 0]])]
 
-    # split_sections = coords[0][0].bincount().tolist()  # split_sections[i]表示的是spike[i]中的脉冲数量（稀疏矩阵索引的数量）
     coords = torch.cat(coords).t().int()
-    feats = torch.ones(size=[coords.shape[0], 1], dtype=torch.float, device=spike.device)
+    feats = torch.ones(size=[coords.shape[0], 1], dtype=torch.float, device=x.device)
     return coords, feats
 
 class SparseConverter:
@@ -49,46 +52,49 @@ class SparseConverter:
     def __init__(self):
         self.index_buffer = {}  # 保存生成索引的tensor的缓冲区
 
-    def to_sparse(self, spike: torch.BoolTensor):
-        assert spike.dim() >= 2, 'spike must be a tensor with more than 1 dim'
+    def to_sparse(self, x: torch.Tensor):
+        assert x.dim() >= 2, 'x must be a tensor with more than 1 dim'
+        if x.dtype == torch.bool:
+            mask = x
+        else:
+            mask = x > 0
 
-        str_shape = SparseConverter.str_tensor_shape(spike)
+        str_shape = SparseConverter.str_tensor_shape(x)
 
         coords = []
-        for i in range(spike.dim()):
+        for i in range(x.dim()):
             # 生成脉冲数据的第i维的索引
             str_shape_i = str_shape + '_' + str(i)
 
             if str_shape_i not in self.index_buffer:
-                index_shape = [1] * spike.dim()
-                index_shape[i] = spike.shape[i]
+                index_shape = [1] * x.dim()
+                index_shape[i] = x.shape[i]
 
-                index_tensor = torch.arange(0, spike.shape[i], device=spike.device).view(index_shape)
+                index_tensor = torch.arange(0, x.shape[i], device=x.device).view(index_shape)
 
-                repeat_shape = list(spike.shape)
+                repeat_shape = list(x.shape)
                 repeat_shape[i] = 1
                 self.index_buffer[str_shape_i] = index_tensor.repeat(repeat_shape)
 
-            coords.append(self.index_buffer[str_shape_i][spike].unsqueeze_(0))
+            coords.append(self.index_buffer[str_shape_i][mask].unsqueeze_(0))
 
-            # 若spike为 tensor([[ True, False,  True],
+            # 若x为 tensor([[ True, False,  True],
             #                  [ True, False, False]])
             # 运行到此处，coords为 [tensor([[0, 0, 1]]), tensor([[0, 2, 0]])]
 
-            # split_sections = coords[0][0].bincount().tolist()  # split_sections[i]表示的是spike[i]中的脉冲数量（稀疏矩阵索引的数量）
         coords = torch.cat(coords).t().int()
-        feats = torch.ones(size=[coords.shape[0], 1], dtype=torch.float, device=spike.device)
+        feats = torch.ones(size=[coords.shape[0], 1], dtype=torch.float, device=x.device)
         return coords, feats
 
 class SparseSpikesMaxPool3d(ME.MinkowskiMaxPooling):
-    def __init__(kernel_size, stride=1, dilation=1):
+    def __init__(self, kernel_size, stride=1, dilation=1):
         super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=4)
 
 class SparseSpikesMaxPool2d(ME.MinkowskiMaxPooling):
-    def __init__(kernel_size, stride=1, dilation=1):
+    def __init__(self, kernel_size, stride=1, dilation=1):
         super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=3)
 
 class SparseSpikesMaxPool1d(ME.MinkowskiMaxPooling):
-    def __init__(kernel_size, stride=1, dilation=1):
+    def __init__(self, kernel_size, stride=1, dilation=1):
         super().__init__(kernel_size=kernel_size, stride=stride, dilation=dilation, dimension=2)
 
