c61ee0611899c32d59f32b15a9ea07a1516d9fa0
fangwei123456 <fangwei123456@pku.edu.cn>
2020-12-12 20:04:05 +0800

    在windows环境下默认不编译cuda扩展

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import torch
 from setuptools import find_packages
 from setuptools import setup
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension, BuildExtension
-
+import sys
 
 requirements = ["torch"]
 
@@ -41,28 +41,51 @@ with open("./requirements.txt", "r", encoding="utf-8") as fh:
 with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
-    install_requires=install_requires,
-    name="spikingjelly",
-    version="0.0.0.0.002",
-    author="PKU MLG, PCL, and other contributors",
-    author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
-    description="A deep learning framework for SNNs built on PyTorch.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/fangwei123456/spikingjelly",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: Open-Intelligence Open Source License 1.0",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    ext_modules=get_extensions(),
-    cmdclass={
-        "build_ext": BuildExtension
-    }
-)
+if sys.platform == 'win32':
+    setup(
+        install_requires=install_requires,
+        name="spikingjelly",
+        version="0.0.0.0.002",
+        author="PKU MLG, PCL, and other contributors",
+        author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
+        description="A deep learning framework for SNNs built on PyTorch.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/fangwei123456/spikingjelly",
+        packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: Open-Intelligence Open Source License 1.0",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6'
+    )
+else:
+    setup(
+        install_requires=install_requires,
+        name="spikingjelly",
+        version="0.0.0.0.002",
+        author="PKU MLG, PCL, and other contributors",
+        author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
+        description="A deep learning framework for SNNs built on PyTorch.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/fangwei123456/spikingjelly",
+        packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: Open-Intelligence Open Source License 1.0",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        ext_modules=get_extensions(),
+        cmdclass={
+            "build_ext": BuildExtension
+        }
+    )
