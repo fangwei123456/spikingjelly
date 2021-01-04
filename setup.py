c4b0357419d94da119d6f2847cd0e5cb9d30965f
fangwei123456 <fangwei123456@pku.edu.cn>
2021-01-04 16:23:31 +0800

    支持windows下安装cuda扩展

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ python -m twine upload dist/*
 import setuptools
 import glob
 import os
- 
-import torch
+
 from setuptools import find_packages
 from setuptools import setup
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension, BuildExtension
@@ -16,12 +15,21 @@ requirements = ["torch"]
 
 
 def get_extensions():
+    if os.environ.get('CUDA_PATH') is None and os.environ.get('CUDA_HOME') is None:
+        print('CUDA_PATH or CUDA_HOME is None. Install Without CUDA Extension')
+        return None
+    else:
+        print('Install With CUDA Extension')
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "spikingjelly", "cext", "csrc")
+    extensions_dir = os.path.join(this_dir, 'spikingjelly', 'cext', 'csrc')
+
+    if sys.platform == 'win32':
+        # win32 does not support cuSparse
+        ext_list = ['neuron']
+    else:
+        ext_list = ['gemm', 'neuron']
     
-    ext_list = ['gemm', 'neuron']
-    
-    extra_compile_args={'cxx': ['-g'], 'nvcc': ['-use_fast_math']}
+    extra_compile_args = {'cxx': ['-g'], 'nvcc': ['-use_fast_math']}
     
     extension = CUDAExtension
     define_macros = [("WITH_CUDA", None)]
@@ -41,51 +49,30 @@ with open("./requirements.txt", "r", encoding="utf-8") as fh:
 with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-if sys.platform == 'win32':
-    setup(
-        install_requires=install_requires,
-        name="spikingjelly",
-        version="0.0.0.0.002",
-        author="PKU MLG, PCL, and other contributors",
-        author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
-        description="A deep learning framework for SNNs built on PyTorch.",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/fangwei123456/spikingjelly",
-        packages=find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3 :: Only",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "License :: Open-Intelligence Open Source License 1.0",
-            "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6'
-    )
-else:
-    setup(
-        install_requires=install_requires,
-        name="spikingjelly",
-        version="0.0.0.0.002",
-        author="PKU MLG, PCL, and other contributors",
-        author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
-        description="A deep learning framework for SNNs built on PyTorch.",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/fangwei123456/spikingjelly",
-        packages=find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3 :: Only",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "License :: Open-Intelligence Open Source License 1.0",
-            "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6',
-        ext_modules=get_extensions(),
-        cmdclass={
-            "build_ext": BuildExtension
-        }
-    )
+
+
+setup(
+    install_requires=install_requires,
+    name="spikingjelly",
+    version="0.0.0.0.003",
+    author="PKU MLG, PCL, and other contributors",
+    author_email="fwei@pku.edu.cn, chyq@pku.edu.cn",
+    description="A deep learning framework for SNNs built on PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fangwei123456/spikingjelly",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: Open-Intelligence Open Source License 1.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    ext_modules=get_extensions(),
+    cmdclass={
+        "build_ext": BuildExtension
+    }
+)
