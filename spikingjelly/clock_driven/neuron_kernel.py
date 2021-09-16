53a7ca30af3fd4f2572449f9dbc8c01b0ee3c5fb
fangwei123456 <fangwei123456@pku.edu.cn>
2021-09-16 17:42:43 +0800

    fix https://github.com/fangwei123456/spikingjelly/issues/106

--- spikingjelly/clock_driven/neuron_kernel.py
+++ spikingjelly/clock_driven/neuron_kernel.py
@@ -252,21 +252,23 @@ try:
                 if v_reset is None:
                     cp_v_reset = None
                     hard_reset = False
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_neuron_num, cp_numel]
                 else:
                     cp_v_reset = cupy.asarray(v_reset, dtype=cp_dtype)
                     hard_reset = True
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel]
 
 
                 kernel = MultiStepIFNodePTT.create_fptt_kernel(hard_reset, dtype)
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
+
                 kernel(
                     (blocks,), (threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
@@ -307,16 +309,17 @@ try:
             with cupy.cuda.Device(device):
 
                 if hard_reset:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel]
                 else:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel]
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
                 kernel(
                     (ctx.blocks,), (ctx.threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
@@ -582,20 +585,21 @@ try:
                 if v_reset is None:
                     cp_v_reset = None
                     hard_reset = False
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel]
                 else:
                     cp_v_reset = cupy.asarray(v_reset, dtype=cp_dtype)
                     hard_reset = True
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel]
 
                 kernel = MultiStepLIFNodePTT.create_fptt_kernel(hard_reset, dtype)
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
                 kernel(
                     (blocks,), (threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
@@ -638,16 +642,18 @@ try:
             with cupy.cuda.Device(device):
 
                 if hard_reset:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel]
                 else:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, grad_x_seq, grad_v_last, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel]
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
+
                 kernel(
                     (ctx.blocks,), (ctx.threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
@@ -964,20 +970,21 @@ try:
                 if v_reset is None:
                     cp_v_reset = None
                     hard_reset = False
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_neuron_num, cp_numel]
                 else:
                     cp_v_reset = cupy.asarray(v_reset, dtype=cp_dtype)
                     hard_reset = True
+                    x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel = cu_kernel_opt.get_contiguous(x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel)
                     kernel_args = [x_seq, v_v_seq, h_seq, spike_seq, cp_reciprocal_tau, cp_v_threshold, cp_v_reset, cp_neuron_num, cp_numel]
 
                 kernel = MultiStepParametricLIFNodePTT.create_fptt_kernel(hard_reset, dtype)
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
                 kernel(
                     (blocks,), (threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
@@ -1021,16 +1028,17 @@ try:
             with cupy.cuda.Device(device):
 
                 if hard_reset:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_v_reset, ctx.cp_neuron_num, ctx.cp_numel]
                 else:
+                    grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel = cu_kernel_opt.get_contiguous(grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel)
                     kernel_args = [grad_spike_seq, grad_v_seq, h_seq, spike_seq, v_v_seq, grad_x_seq, grad_v_last, grad_reciprocal_tau, ctx.cp_reciprocal_tau, ctx.cp_one_sub_reciprocal_tau, ctx.cp_v_threshold, ctx.cp_neuron_num, ctx.cp_numel]
 
-                kernel_args = cu_kernel_opt.get_contiguous(*kernel_args)
                 kernel(
                     (ctx.blocks,), (ctx.threads,),
                     cu_kernel_opt.wrap_args_to_raw_kernel(
                         device,
-                        kernel_args
+                        *kernel_args
                     )
                 )
 
