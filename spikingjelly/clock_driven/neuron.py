add decay_input option for LIF/PLIF. The gradients of tau for PLIF are wrong in fp16 and need to be fixed.

spikingjelly/clock_driven/neuron.py
@@ -171,7 +171,6 @@ class BaseNode(base.MemoryModule):
         self.neuronal_reset()
         return self.spike
 
-
 class IFNode(BaseNode):
     def __init__(self, v_threshold: float = 1., v_reset: float = 0.,
                  surrogate_function: Callable = surrogate.Sigmoid(), detach_reset: bool = False):
@@ -225,7 +224,6 @@ class IFNode(BaseNode):
 
     def neuronal_charge(self, x: torch.Tensor):
         self.v = self.v + x
-
 
 class MultiStepIFNode(IFNode):
     def __init__(self, v_threshold: float = 1., v_reset: float = 0.,
@@ -345,7 +343,7 @@ class MultiStepIFNode(IFNode):
         return super().extra_repr() + f', backend={self.backend}'
 
 class LIFNode(BaseNode):
-    def __init__(self, tau: float = 2., v_threshold: float = 1.,
+    def __init__(self, tau: float = 2., decay_input: bool = True, v_threshold: float = 1.,
                  v_reset: float = 0., surrogate_function: Callable = surrogate.Sigmoid(),
                  detach_reset: bool = False):
         """
@@ -373,7 +371,7 @@ class LIFNode(BaseNode):
         Leaky Integrate-and-Fire 神经元模型，可以看作是带漏电的积分器。其阈下神经动力学方程为：
 
         .. math::
-            V[t] = V[t-1] + \\frac{1}{\\tau}(X[t] - (V[t-1] - V_{reset})
+            V[t] = V[t-1] + \\frac{1}{\\tau}(X[t] - (V[t-1] - V_{reset}))
 
         * :ref:`中文API <LIFNode.__init__-cn>`
 
@@ -399,28 +397,32 @@ class LIFNode(BaseNode):
         The subthreshold neural dynamics of it is as followed:
 
         .. math::
-            V[t] = V[t-1] + \\frac{1}{\\tau}(X[t] - (V[t-1] - V_{reset})
+            V[t] = V[t-1] + \\frac{1}{\\tau}(X[t] - (V[t-1] - V_{reset}))
         """
         assert isinstance(tau, float) and tau > 1.
 
         super().__init__(v_threshold, v_reset, surrogate_function, detach_reset)
         self.tau = tau
+        self.decay_input = decay_input
 
     def extra_repr(self):
         return super().extra_repr() + f', tau={self.tau}'
 
     def neuronal_charge(self, x: torch.Tensor):
-        if self.v_reset is None:
-            self.v = self.v + (x - self.v) / self.tau
-
-        else:
-            if isinstance(self.v_reset, float) and self.v_reset == 0.:
+        if self.decay_input:
+            if self.v_reset is None or self.v_reset == 0.:
                 self.v = self.v + (x - self.v) / self.tau
             else:
                 self.v = self.v + (x - (self.v - self.v_reset)) / self.tau
 
+        else:
+            if self.v_reset is None or self.v_reset == 0.:
+                self.v = self.v * (1. - 1. / self.tau) + x
+            else:
+                self.v = self.v - (self.v - self.v_reset) / self.tau + x
+
 class MultiStepLIFNode(LIFNode):
-    def __init__(self, tau: float = 2., v_threshold: float = 1.,
+    def __init__(self, tau: float = 2., decay_input: bool = True, v_threshold: float = 1.,
                  v_reset: float = 0., surrogate_function: Callable = surrogate.Sigmoid(),
                  detach_reset: bool = False, backend='torch'):
         """
@@ -497,7 +499,7 @@ class MultiStepLIFNode(LIFNode):
             and multi-step propagation.
 
         """
-        super().__init__(tau, v_threshold, v_reset, surrogate_function, detach_reset)
+        super().__init__(tau, decay_input, v_threshold, v_reset, surrogate_function, detach_reset)
         self.register_memory('v_seq', None)
         self.register_memory('spike_seq', None)
 
@@ -526,7 +528,7 @@ class MultiStepLIFNode(LIFNode):
 
 
             self.spike_seq, self.v_seq = neuron_kernel.MultiStepLIFNodePTT.apply(
-                x_seq.flatten(1), self.v.flatten(0), self.tau, self.v_threshold, self.v_reset, self.detach_reset, self.surrogate_function.cuda_code)
+                x_seq.flatten(1), self.v.flatten(0), self.decay_input, self.tau, self.v_threshold, self.v_reset, self.detach_reset, self.surrogate_function.cuda_code)
 
             self.spike_seq = self.spike_seq.reshape(x_seq.shape)
             self.v_seq = self.v_seq.reshape(x_seq.shape)
@@ -542,7 +544,7 @@ class MultiStepLIFNode(LIFNode):
         return super().extra_repr() + f', backend={self.backend}'
 
 class ParametricLIFNode(BaseNode):
-    def __init__(self, init_tau: float = 2.0, v_threshold: float = 1.,
+    def __init__(self, init_tau: float = 2.0, decay_input: bool = True, v_threshold: float = 1.,
                  v_reset: float = 0., surrogate_function: Callable = surrogate.Sigmoid(),
                  detach_reset: bool = False):
         """
@@ -605,6 +607,7 @@ class ParametricLIFNode(BaseNode):
 
         assert isinstance(init_tau, float) and init_tau > 1.
         super().__init__(v_threshold, v_reset, surrogate_function, detach_reset)
+        self.decay_input = decay_input
         init_w = - math.log(init_tau - 1.)
         self.w = nn.Parameter(torch.as_tensor(init_w))
 
@@ -614,17 +617,19 @@ class ParametricLIFNode(BaseNode):
         return super().extra_repr() + f', tau={tau}'
 
     def neuronal_charge(self, x: torch.Tensor):
-        if self.v_reset is None:
-            self.v = self.v + (x - self.v) * self.w.sigmoid()
-        else:
-            if self.v_reset == 0.:
+        if self.decay_input:
+            if self.v_reset is None or self.v_reset == 0.:
                 self.v = self.v + (x - self.v) * self.w.sigmoid()
             else:
                 self.v = self.v + (x - (self.v - self.v_reset)) * self.w.sigmoid()
-
+        else:
+            if self.v_reset is None or self.v_reset == 0.:
+                self.v = self.v * (1. - self.w.sigmoid()) + x
+            else:
+                self.v = self.v - (self.v - self.v_reset) * self.w.sigmoid() + x
 
 class MultiStepParametricLIFNode(ParametricLIFNode):
-    def __init__(self, init_tau: float = 2., v_threshold: float = 1.,
+    def __init__(self, init_tau: float = 2., decay_input: bool = True, v_threshold: float = 1.,
                  v_reset: float = 0., surrogate_function: Callable = surrogate.Sigmoid(),
                  detach_reset: bool = False, backend='torch'):
         """
@@ -709,7 +714,7 @@ class MultiStepParametricLIFNode(ParametricLIFNode):
             Read :doc:`Propagation Pattern <./clock_driven_en/10_propagation_pattern>` for more details about single-step
             and multi-step propagation.
         """
-        super().__init__(init_tau, v_threshold, v_reset, surrogate_function, detach_reset)
+        super().__init__(init_tau, decay_input, v_threshold, v_reset, surrogate_function, detach_reset)
         self.register_memory('v_seq', None)
         self.register_memory('spike_seq', None)
 
@@ -738,7 +743,7 @@ class MultiStepParametricLIFNode(ParametricLIFNode):
 
 
             self.spike_seq, self.v_seq = neuron_kernel.MultiStepParametricLIFNodePTT.apply(
-                x_seq.flatten(1), self.v.flatten(0), self.w.sigmoid(), self.v_threshold, self.v_reset, self.detach_reset, self.surrogate_function.cuda_code)
+                x_seq.flatten(1), self.v.flatten(0), self.w.sigmoid(), self.decay_input, self.v_threshold, self.v_reset, self.detach_reset, self.surrogate_function.cuda_code)
 
             self.spike_seq = self.spike_seq.reshape(x_seq.shape)
             self.v_seq = self.v_seq.reshape(x_seq.shape)