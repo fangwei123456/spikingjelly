c7f0b9316883d684f1bbe7634ea1e9e23928e529
fangwei123456 <fangwei123456@pku.edu.cn>
2022-01-13 22:38:32 +0800

    add gpu monitor

--- spikingjelly/clock_driven/cu_kernel_opt.py
+++ spikingjelly/clock_driven/cu_kernel_opt.py
@@ -4,7 +4,9 @@ try:
     import time
     import numpy as np
     from .. import configure
-
+    import os
+    import threading
+    import datetime
 
     def cal_fun_t(n, device, f, *args, **kwargs):
         if n <= 2:
@@ -62,6 +64,47 @@ try:
                 raise TypeError
 
         return tuple(ret_list)
+
+    class GPUMonitor(threading.Thread):
+        def __init__(self, gpu_ids: list = None, interval: float = 10., output_file: str or None = None, start_now=True):
+            super().__init__()
+            self.gpu_ids = gpu_ids
+            self.interval = interval
+            self.stopped = False
+            self.output_file = output_file
+
+            self.cmds = 'nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv'
+            if self.gpu_ids is not None:
+                self.cmds += ' -i '
+                id_str = []
+                for gpu_id in self.gpu_ids:
+                    id_str.append(str(gpu_id))
+                self.cmds += ','.join(id_str)
+
+            if start_now:
+                self.start()
+
+        def stop(self):
+            self.stopped = True
+
+        def run(self):
+            while not self.stopped:
+                with os.popen(self.cmds) as fp:
+                    outputs = fp.read()
+                    if self.output_file is not None:
+                        with open(self.output_file, 'a+', encoding='utf-8') as output_file:
+                            output_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+                            output_file.write(outputs)
+                    else:
+                        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        print(outputs)
+
+                time.sleep(self.interval)
+
+
+
+
+
 
 except ImportError:
     pass
