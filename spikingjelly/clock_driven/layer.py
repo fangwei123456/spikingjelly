3aa8d70a4588a63dfc15fe4708df8e46a79e8d7f
Gausx <gaohuanh@gmail.com>
2022-04-01 09:10:21 +0800

    ✨ feat(Layer):
    add TemporalWiseAttention

--- spikingjelly/clock_driven/layer.py
+++ spikingjelly/clock_driven/layer.py
@@ -1385,3 +1385,82 @@ class SpikeConv3d(nn.Conv3d):
         return functional.spike_conv3d(
             spike, weight, bias, self.stride, self.padding, self.dilation, self.groups
         )
+
+class MutiStepTemporalWiseAttention(nn.Module):
+    def __init__(self, T:int, reduction:int = 16, dimension:int = 4):
+        """
+        * :ref:`API in English <MutiStepTemporalWiseAttention.__init__-en>`
+
+        .. _MutiStepTemporalWiseAttention.__init__-cn:
+
+        :param T: 输入数据的时间步长
+
+        :param reduction: 压缩比
+
+        :param dimension: 输入数据的维度。当输入数据为[T, N, C, H, W]时， dimension = 4；输入数据维度为[T, N, L]时，dimension = 2。
+
+        `Temporal-Wise Attention Spiking Neural Networks for Event Streams Classification <https://openaccess.thecvf.com/content/ICCV2021/html/Yao_Temporal-Wise_Attention_Spiking_Neural_Networks_for_Event_Streams_Classification_ICCV_2021_paper.html>`_ 中提出
+        的MutiStepTemporalWiseAttention层。MutiStepTemporalWiseAttention层必须放在二维卷积层之后脉冲神经元之前，例如：
+
+        ``Conv2d -> MutiStepTemporalWiseAttention -> LIF``
+
+        输入的尺寸是 ``[T, N, C, H, W]`` 或者 ``[T, N, L]`` ，经过MutiStepTemporalWiseAttention层，输出为``[T, N, C, H, W]`` 或者 ``[T, N, L]`` 。
+
+        ``reduction`` 是压缩比，相当于论文中的 :math:`r`。
+
+        * :ref:`中文API <MutiStepTemporalWiseAttention.__init__-cn>`
+
+        .. _MutiStepTemporalWiseAttention.__init__-en:
+
+        :param T: timewindows of input
+
+        :param reduction: reduction ratio
+
+        :param dimension: Dimensions of input. If the input dimension is [T, N, C, H, W], dimension = 4; when the input dimension is [T, N, L], dimension = 2.
+
+        The MutiStepTemporalWiseAttention layer is proposed in Temporal-Wise Attention Spiking Neural Networks for Event Streams Classification <https://openaccess.thecvf.com/content/ICCV2021/html/Yao_Temporal-Wise_Attention_Spiking_Neural_Networks_for_Event_Streams_Classification_ICCV_2021_paper.html>`_.
+
+        It should be placed after the convolution layer and before the spiking neurons, e.g.,
+
+        ``Conv2d -> MutiStepTemporalWiseAttention -> LIF``
+
+        The dimension of the input is ``[T, N, C, H, W]`` or  ``[T, N, L]`` , after the MutiStepTemporalWiseAttention layer, the output dimension is ``[T, N, C, H, W]`` or  ``[T, N, L]`` .
+
+        ``reduction`` is the reduction ratio，which is :math:`r` in the paper.
+
+        """
+        super().__init__()
+        assert dimension == 4 or dimension == 2, 'dimension must be 4 or 2'
+
+        self.dimension = dimension
+        
+        # Sequence
+        if self.dimension == 2:
+            self.avg_pool = nn.AdaptiveAvgPool1d(1)
+            self.max_pool = nn.AdaptiveMaxPool1d(1)
+        elif self.dimension == 4:
+            self.avg_pool = nn.AdaptiveAvgPool3d(1)
+            self.max_pool = nn.AdaptiveMaxPool3d(1)
+        
+        assert T >= reduction, 'reduction cannot be greater than T'
+
+        # Excitation
+        self.sharedMLP = nn.Sequential(
+            nn.Linear(T, T // reduction, bias=False), 
+            nn.ReLU(),
+            nn.Linear(T // reduction, T, bias=False)
+            )
+            
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = x.transpose(0,1)
+        avgout = self.sharedMLP(self.avg_pool(x).view([x.shape[0], x.shape[1]]))
+        maxout = self.sharedMLP(self.max_pool(x).view([x.shape[0], x.shape[1]]))
+        scores = self.sigmoid(avgout + maxout)
+        if self.dimension == 2:
+            out =  x * scores[:, :, None]
+        elif self.dimension == 4:
+            out =  x * scores[:, :, None, None, None]
+        out = out.transpose(0,1)
+        return out
