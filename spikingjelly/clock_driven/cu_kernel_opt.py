4cec7a5c96ff1970f8fbe845adf7582b982e06cf
fangwei123456 <fangwei123456@pku.edu.cn>
2022-04-28 11:23:59 +0800

    use tensorboard to recode

--- spikingjelly/clock_driven/cu_kernel_opt.py
+++ spikingjelly/clock_driven/cu_kernel_opt.py
@@ -6,7 +6,8 @@ from .. import configure
 import os
 import threading
 import datetime
-
+from torch.utils.tensorboard import SummaryWriter
+import re
 try:
     import cupy
 except BaseException as e:
@@ -70,20 +71,58 @@ def wrap_args_to_raw_kernel(device: int, *args):
     return tuple(ret_list)
 
 class GPUMonitor(threading.Thread):
-    def __init__(self, gpu_ids: list = None, interval: float = 10., output_file: str or None = None, start_now=True):
+    def __init__(self, log_dir: str = None, gpu_ids: tuple = (0, ), interval: float = 60., start_now=True):
+        """
+        :param log_dir: the directory for saving logs with tensorboard. If it is None, this module will print logs
+        :type log_dir: str
+        :param gpu_ids: the id of GPUs to be monitored, e.g., `(0, 1, 2, 3)`. The default value is `(0, )`
+        :type gpu_ids: tuple
+        :param interval: the recording interval (in seconds)
+        :type interval: float
+        :param start_now: if true, the monitor will start to record now. Otherwise, it will start after the user call `start()` manually
+        :type start_now:
+
+        The GPU monitor, which starts a new thread to record the utilization and memory used of `gpu_ids` every `interval` seconds.
+
+        .. admonition:: Warning
+        :class: warning
+
+            Do not forget to call `stop()` after the main thread finishes its job, otherwise the main thread will never stop!
+
+        Codes example:
+
+        .. code-block:: python
+
+            import time
+
+            gm = GPUMonitor(interval=1)
+            time.sleep(2)  # make the main thread sleep
+            gm.stop()
+
+            # The outputs are:
+
+            # 2022-04-28 10:52:25
+            # utilization.gpu [%], memory.used [MiB]
+            # 0 %, 376 MiB
+        """
         super().__init__()
         self.gpu_ids = gpu_ids
         self.interval = interval
         self.stopped = False
-        self.output_file = output_file
-
         self.cmds = 'nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv'
-        if self.gpu_ids is not None:
-            self.cmds += ' -i '
-            id_str = []
-            for gpu_id in self.gpu_ids:
-                id_str.append(str(gpu_id))
-            self.cmds += ','.join(id_str)
+        self.cmds += ' -i '
+        id_str = []
+        for gpu_id in self.gpu_ids:
+            id_str.append(str(gpu_id))
+        self.cmds += ','.join(id_str)
+        self.step = 0
+
+        if log_dir is None:
+            self.writer = None
+        else:
+            self.writer = []
+            for i in range(self.gpu_ids.__len__()):
+                self.writer.append(SummaryWriter(os.path.join(log_dir, f'gpu_{id_str[i]}')))
 
         if start_now:
             self.start()
@@ -95,15 +134,28 @@ class GPUMonitor(threading.Thread):
         while not self.stopped:
             with os.popen(self.cmds) as fp:
                 outputs = fp.read()
-                if self.output_file is not None:
-                    with open(self.output_file, 'a+', encoding='utf-8') as output_file:
-                        output_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
-                        output_file.write(outputs)
+                if self.writer is not None:
+                    outputs = outputs.split('\n')[1:-1]
+                    # skip the first row 'utilization.gpu [%], memory.used [MiB]' and the last row ('\n')
+                    for i in range(outputs.__len__()):
+                        utilization_memory = re.findall(r'\d+', outputs[i])
+                        utilization = int(utilization_memory[0])
+                        memory_used = int(utilization_memory[1])
+                        self.writer[i].add_scalar('utilization', utilization, self.step)
+                        self.writer[i].add_scalar('memory_used', memory_used, self.step)
                 else:
                     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     print(outputs)
-
+                    '''
+                    2022-04-20 18:14:26
+                    utilization.gpu [%], memory.used [MiB]
+                    4 %, 1816 MiB
+                    0 %, 1840 MiB
+                    0 %, 1840 MiB
+                    0 %, 1720 MiB
+                    '''
             time.sleep(self.interval)
+            self.step += 1
 
 
 class DeviceEnvironment:
