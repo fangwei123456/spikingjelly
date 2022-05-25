6139510c5004ca806639e925795e44e1a70a960f
fangwei123456 <fangwei123456@pku.edu.cn>
2022-05-25 22:09:59 +0800

    add classify dvsg tutorial

--- docs/source/_static/tutorials/activation_based/classify_dvsg/plot_logs.py
+++ docs/source/_static/tutorials/activation_based/classify_dvsg/plot_logs.py
@@ -20,16 +20,16 @@ def plot_log(csv_file, x_label=None, y_label=None, plot_max=False, label=None, f
 
 if __name__ == '__main__':
     plt.style.use(['science'])
-    root = 'C:/Users/fw/Desktop/代码/spikingjelly/docs/source/_static/tutorials/activation_based/conv_fashion_mnist'
+    root = 'C:/Users/fw/Desktop/代码/spikingjelly/docs/source/_static/tutorials/activation_based/classify_dvsg'
     figsize = (12, 8)
     dpi = 200
     fontsize = 20
     fig = plt.figure(figsize=figsize, dpi=dpi)
 
-    title = 'Training curves on FashionMNIST'
+    title = 'Training curves on DVS Gesture'
     fname_label_xlabel_ylabel = {
-        'run-T4_b256_sgd_lr0.1_c128_amp_cupy-tag-train_acc.csv': ('Training accuracy', 'epoch', 'accuracy'),
-        'run-T4_b256_sgd_lr0.1_c128_amp_cupy-tag-test_acc.csv': ('Test accuracy', 'epoch', 'accuracy'),
+        'run-T16_b16_adam_lr0.001_c128_amp_cupy-tag-train_acc.csv': ('Training accuracy', 'epoch', 'accuracy'),
+        'run-T16_b16_adam_lr0.001_c128_amp_cupy-tag-test_acc.csv': ('Test accuracy', 'epoch', 'accuracy'),
     }
 
     for fname in fname_label_xlabel_ylabel.keys():
@@ -37,6 +37,6 @@ if __name__ == '__main__':
         plot_log(os.path.join(root, fname), x_label=x_label, y_label=y_label, label=label, plot_max= (label == 'Test accuracy'))
     plt.title(title, fontsize=fontsize)
     plt.legend(frameon=True, fontsize=fontsize)
-    plt.savefig(os.path.join(root, f'fmnist_logs.pdf'))
-    plt.savefig(os.path.join(root, f'fmnist_logs.svg'))
-    plt.savefig(os.path.join(root, f'fmnist_logs.png'))
+    plt.savefig(os.path.join(root, f'dvsg_logs.pdf'))
+    plt.savefig(os.path.join(root, f'dvsg_logs.svg'))
+    plt.savefig(os.path.join(root, f'dvsg_logs.png'))
