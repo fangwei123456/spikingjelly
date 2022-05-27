d47f0f595d33af8277b5d52aab4dbf0b1b530966
fangwei123456 <fangwei123456@pku.edu.cn>
2022-05-27 15:54:59 +0800

    add tutorial about train large SNNs

--- spikingjelly/activation_based/layer.py
+++ spikingjelly/activation_based/layer.py
@@ -141,6 +141,31 @@ class BatchNorm2d(nn.BatchNorm2d, base.StepModule):
                 raise ValueError(f'expected x with shape [T, N, C, H, W], but got x with shape {x.shape}!')
             return functional.seq_to_ann_forward(x, super().forward)
 
+class GroupNorm(nn.GroupNorm, base.StepModule):
+    def __init__(
+            self,
+            num_groups: int, num_channels: int, eps: float = 1e-5, affine: bool = True,
+            step_mode='s'
+    ):
+        """
+        :param step_mode: the step mode, which can be `s` (single-step) or `m` (multi-step)
+        :type step_mode: str
+
+        Refer to :class:`torch.nn.BatchNorm2d` for other parameters' information.
+        """
+        super().__init__(num_groups, num_channels, eps, affine)
+        self.step_mode = step_mode
+
+    def extra_repr(self):
+        return super().extra_repr() + f', step_mode={self.step_mode}'
+
+    def forward(self, x: Tensor):
+        if self.step_mode == 's':
+            return super().forward(x)
+
+        elif self.step_mode == 'm':
+            return functional.seq_to_ann_forward(x, super().forward)
+
 
 class MaxPool2d(nn.MaxPool2d, base.StepModule):
     def __init__(self, kernel_size: _size_any_t, stride: Optional[_size_any_t] = None,
@@ -163,6 +188,44 @@ class MaxPool2d(nn.MaxPool2d, base.StepModule):
 
         return x
 
+class AvgPool2d(nn.AvgPool2d, base.StepModule):
+    def __init__(self, kernel_size: _size_2_t, stride: Optional[_size_2_t] = None, padding: _size_2_t = 0,
+                 ceil_mode: bool = False, count_include_pad: bool = True, divisor_override: Optional[int] = None, step_mode='s') -> None:
+        super().__init__(kernel_size, stride, padding, ceil_mode, count_include_pad, divisor_override)
+        self.step_mode = step_mode
+
+    def extra_repr(self):
+        return super().extra_repr() + f', step_mode={self.step_mode}'
+
+    def forward(self, x: Tensor):
+        if self.step_mode == 's':
+            x = super().forward(x)
+
+        elif self.step_mode == 'm':
+            if x.dim() != 5:
+                raise ValueError(f'expected x with shape [T, N, C, H, W], but got x with shape {x.shape}!')
+            x = functional.seq_to_ann_forward(x, super().forward)
+
+        return x
+
+class AdaptiveAvgPool2d(nn.AdaptiveAvgPool2d, base.StepModule):
+    def __init__(self, output_size, step_mode='s') -> None:
+        super().__init__(output_size)
+        self.step_mode = step_mode
+
+    def extra_repr(self):
+        return super().extra_repr() + f', step_mode={self.step_mode}'
+
+    def forward(self, x: Tensor):
+        if self.step_mode == 's':
+            x = super().forward(x)
+
+        elif self.step_mode == 'm':
+            if x.dim() != 5:
+                raise ValueError(f'expected x with shape [T, N, C, H, W], but got x with shape {x.shape}!')
+            x = functional.seq_to_ann_forward(x, super().forward)
+
+        return x
 
 class Linear(nn.Linear, base.StepModule):
     def __init__(self, in_features: int, out_features: int, bias: bool = True, step_mode='s') -> None:
---
+++ spikingjelly/activation_based/model/train_imagenet_example.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from spikingjelly.activation_based import surrogate, neuron, functional
+from spikingjelly.activation_based.model import spiking_resnet, train_classify
+
+
+class SResNetTrainer(train_classify.Trainer):
+    def cal_acc1_acc5(self, output, target):
+        # output.shape = [T, N, C]
+        return super().cal_acc1_acc5(output.mean(0), target)
+
+    def preprocess_train_sample(self, args, x: torch.Tensor):
+        # define how to process train sample before send it to model
+        return x.unsqueeze(0).repeat(args.T, 1, 1, 1, 1)  # [N, C, H, W] -> [T, N, C, H, W]
+
+    def preprocess_test_sample(self, args, x: torch.Tensor):
+        # define how to process test sample before send it to model
+        return x.unsqueeze(0).repeat(args.T, 1, 1, 1, 1)  # [N, C, H, W] -> [T, N, C, H, W]
+
+    def get_args_parser(self, add_help=True):
+        parser = super().get_args_parser()
+        parser.add_argument('--T', type=str, help="total time-steps")
+        parser.add_argument('--cupy', action="store_true", help="set the neurons to use cupy backend")
+        return parser
+
+    def get_tb_logdir_name(self, args):
+        return super().get_tb_logdir_name(args) + f'_T{args.T}'
+
+    def load_model(self, args, num_classes):
+        if args.model in spiking_resnet.__all__:
+            model = spiking_resnet.__dict__[args.model](pretrained=args.pretrained, spiking_neuron=neuron.IFNode,
+                                                        surrogate_function=surrogate.ATan(), detach_reset=True)
+            functional.set_step_mode(model, step_mode='m')
+            if args.cupy:
+                functional.set_backend(model, 'cupy', neuron.IFNode)
+
+            return model
+        else:
+            raise ValueError(f"args.model should be one of {spiking_resnet.__all__}")
+
+
+if __name__ == "__main__":
+    trainer = SResNetTrainer()
+    args = trainer.get_args_parser().parse_args()
+    trainer.main(args)
+
