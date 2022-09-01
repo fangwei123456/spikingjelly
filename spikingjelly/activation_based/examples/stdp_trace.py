f55ffdce34ff02e79e6f9155f2374d102b32ef15
fangwei123456 <fangwei123456@pku.edu.cn>
2022-09-01 10:38:08 +0800

    fix stdp symbol bug

--- spikingjelly/activation_based/examples/stdp_trace.py
+++ spikingjelly/activation_based/examples/stdp_trace.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from spikingjelly.activation_based import neuron, layer, learning
 from matplotlib import pyplot as plt
+torch.manual_seed(0)
+# plt.style.use(['science'])
 
 if __name__ == '__main__':
     fig = plt.figure(dpi=200, figsize=(8, 6))
@@ -9,19 +11,19 @@ if __name__ == '__main__':
     def f_weight(x):
         return torch.clamp(x, -1, 1.)
 
-    tau_pre = 20.
+    tau_pre = 2.
     tau_post = 100.
-    T = 64
+    T = 128
     N = 1
-    lr = 0.1
+    lr = 0.01
     net = nn.Sequential(
         layer.Linear(1, 1, bias=False),
         neuron.IFNode()
     )
-    nn.init.constant_(net[0].weight.data, 0.2)
+    nn.init.constant_(net[0].weight.data, 0.4)
     optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.)
 
-    in_spike = (torch.rand([T, N, 1]) > 0.8).float()
+    in_spike = (torch.rand([T, N, 1]) > 0.7).float()
     stdp_learner = learning.STDPLearner(step_mode='s', synapse=net[0], sn=net[1], tau_pre=tau_pre, tau_post=tau_post,
                                         f_pre=f_weight, f_post=f_weight)
 
@@ -47,29 +49,30 @@ if __name__ == '__main__':
 
     t = torch.arange(0, T).float()
 
+    cmap = plt.get_cmap('tab10')
     plt.subplot(5, 1, 1)
-    plt.eventplot((in_spike * t)[in_spike == 1], lineoffsets=0, colors='r')
+    plt.eventplot((in_spike * t)[in_spike == 1], lineoffsets=0, colors=cmap(0))
     plt.xlim(-0.5, T + 0.5)
     plt.ylabel('$s[i]$', rotation=0, labelpad=10)
     plt.xticks([])
     plt.yticks([])
 
     plt.subplot(5, 1, 2)
-    plt.plot(t, trace_pre)
+    plt.plot(t, trace_pre, c=cmap(1))
     plt.xlim(-0.5, T + 0.5)
     plt.ylabel('$tr_{pre}$', rotation=0)
     plt.yticks([trace_pre.min().item(), trace_pre.max().item()])
     plt.xticks([])
 
     plt.subplot(5, 1, 3)
-    plt.eventplot((out_spike * t)[out_spike == 1], lineoffsets=0, colors='r')
+    plt.eventplot((out_spike * t)[out_spike == 1], lineoffsets=0, colors=cmap(2))
     plt.xlim(-0.5, T + 0.5)
     plt.ylabel('$s[j]$', rotation=0, labelpad=10)
     plt.xticks([])
     plt.yticks([])
 
     plt.subplot(5, 1, 4)
-    plt.plot(t, trace_post)
+    plt.plot(t, trace_post, c=cmap(3))
     plt.ylabel('$tr_{post}$', rotation=0)
     plt.yticks([trace_post.min().item(), trace_post.max().item()])
 
@@ -77,12 +80,15 @@ if __name__ == '__main__':
     plt.xticks([])
 
     plt.subplot(5, 1, 5)
-    plt.plot(t, weight)
+    plt.plot(t, weight, c=cmap(4))
     plt.xlim(-0.5, T + 0.5)
     plt.ylabel('$w[i][j]$', rotation=0)
     plt.yticks([weight.min().item(), weight.max().item()])
     plt.xlabel('time-step')
     plt.show()
+    # plt.savefig('./docs/source/_static/tutorials/activation_based/stdp/trace.png')
+    # plt.savefig('./docs/source/_static/tutorials/activation_based/stdp/trace.svg')
+    # plt.savefig('./docs/source/_static/tutorials/activation_based/stdp/trace.pdf')
 
 
 
