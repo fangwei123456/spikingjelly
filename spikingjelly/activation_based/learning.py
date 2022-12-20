4eec65ede1e2bdd1a98a6d39f83dddbd017944e2
fangwei123456 <fangwei123456@pku.edu.cn>
2022-12-20 20:50:36 +0800

    add stdp1d; remove w_max/w_min from the args of f_weight. For more details, refer to https://github.com/fangwei123456/spikingjelly/issues/326

--- spikingjelly/activation_based/learning.py
+++ spikingjelly/activation_based/learning.py
@@ -13,7 +13,7 @@ def stdp_linear_single_step(
     fc: nn.Linear, in_spike: torch.Tensor, out_spike: torch.Tensor,
     trace_pre: Union[float, torch.Tensor, None],
     trace_post: Union[float, torch.Tensor, None],
-    tau_pre: float, tau_post: float, w_min: float, w_max: float,
+    tau_pre: float, tau_post: float,
     f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
 ):
     if trace_pre is None:
@@ -27,8 +27,8 @@ def stdp_linear_single_step(
     trace_post = trace_post - trace_post / tau_post + out_spike # shape = [batch_size, N_out]
 
     # [batch_size, N_out, N_in] -> [N_out, N_in]
-    delta_w_pre = -f_pre(weight, w_min) * (trace_post.unsqueeze(2) * in_spike.unsqueeze(1)).sum(0)
-    delta_w_post = f_post(weight, w_max) * (trace_pre.unsqueeze(1) * out_spike.unsqueeze(2)).sum(0)
+    delta_w_pre = -f_pre(weight) * (trace_post.unsqueeze(2) * in_spike.unsqueeze(1)).sum(0)
+    delta_w_post = f_post(weight) * (trace_pre.unsqueeze(1) * out_spike.unsqueeze(2)).sum(0)
     return trace_pre, trace_post, delta_w_pre + delta_w_post
 
 
@@ -36,7 +36,7 @@ def mstdp_linear_single_step(
     fc: nn.Linear, in_spike: torch.Tensor, out_spike: torch.Tensor,
     trace_pre: Union[float, torch.Tensor, None],
     trace_post: Union[float, torch.Tensor, None],
-    tau_pre: float, tau_post: float, w_min: float, w_max: float,
+    tau_pre: float, tau_post: float,
     f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
 ):
     if trace_pre is None:
@@ -50,8 +50,8 @@ def mstdp_linear_single_step(
     trace_post = trace_post * math.exp(-1 / tau_post) + out_spike  # shape = [batch_size, C_out]
 
     # [batch_size, N_out, N_in]
-    eligibility = f_post(weight, w_max) * (trace_pre.unsqueeze(1) * out_spike.unsqueeze(2)) -\
-                  f_pre(weight, w_min) * (trace_post.unsqueeze(2) * in_spike.unsqueeze(1))
+    eligibility = f_post(weight) * (trace_pre.unsqueeze(1) * out_spike.unsqueeze(2)) -\
+                  f_pre(weight) * (trace_post.unsqueeze(2) * in_spike.unsqueeze(1))
     return trace_pre, trace_post, eligibility
 
 
@@ -60,7 +60,6 @@ def mstdpet_linear_single_step(
     trace_pre: Union[float, torch.Tensor, None],
     trace_post: Union[float, torch.Tensor, None],
     tau_pre: float, tau_post: float, tau_trace: float,
-    w_min: float, w_max: float,
     f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
 ):
     if trace_pre is None:
@@ -73,15 +72,15 @@ def mstdpet_linear_single_step(
     trace_pre = trace_pre * math.exp(-1 / tau_pre) + in_spike
     trace_post = trace_post * math.exp(-1 / tau_post) + out_spike
 
-    eligibility = f_post(weight, w_max) * torch.outer(out_spike, trace_pre) -\
-                  f_pre(weight, w_min) * torch.outer(trace_post, in_spike)
+    eligibility = f_post(weight) * torch.outer(out_spike, trace_pre) -\
+                  f_pre(weight) * torch.outer(trace_post, in_spike)
     return trace_pre, trace_post, eligibility
 
 
 def stdp_conv2d_single_step(
     conv: nn.Conv2d, in_spike: torch.Tensor, out_spike: torch.Tensor,
     trace_pre: Union[torch.Tensor, None], trace_post: Union[torch.Tensor, None],
-    tau_pre: float, tau_post: float, w_min: float, w_max: float,
+    tau_pre: float, tau_post: float,
     f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
 ):
     if conv.dilation != (1, 1):
@@ -135,35 +134,104 @@ def stdp_conv2d_single_step(
             tr_pre = trace_pre[:, :, h:h_end:stride_h, w:w_end:stride_w]    # shape = [batch_size, C_in, h_out, w_out]
             tr_post = trace_post    # shape = [batch_size, C_out, h_out, w_out]
 
-            delta_w_pre = - (f_pre(weight, w_min) * 
+            delta_w_pre = - (f_pre(weight) *
 			                (tr_post.unsqueeze(2) * pre_spike.unsqueeze(1))
                             .permute([1, 2, 0, 3, 4]).sum(dim = [2, 3, 4]))
-            delta_w_post = f_post(weight, w_max) * \
+            delta_w_post = f_post(weight) * \
                            (tr_pre.unsqueeze(1) * post_spike.unsqueeze(2))\
                            .permute([1, 2, 0, 3, 4]).sum(dim = [2, 3, 4])
             delta_w[:, :, h, w] += delta_w_pre + delta_w_post
 
     return trace_pre, trace_post, delta_w
 
+def stdp_conv1d_single_step(
+    conv: nn.Conv1d, in_spike: torch.Tensor, out_spike: torch.Tensor,
+    trace_pre: Union[torch.Tensor, None], trace_post: Union[torch.Tensor, None],
+    tau_pre: float, tau_post: float,
+    f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
+):
+    if conv.dilation != (1, ):
+        raise NotImplementedError(
+            'STDP with dilation != 1 for Conv1d has not been implemented!'
+        )
+    if conv.groups != 1:
+        raise NotImplementedError(
+            'STDP with groups != 1 for Conv1d has not been implemented!'
+        )
+
+    stride_l = conv.stride[0]
+
+    if conv.padding == (0, ):
+        pass
+    else:
+        pL = conv.padding[0]
+        if conv.padding_mode != 'zeros':
+            in_spike = F.pad(
+                in_spike, conv._reversed_padding_repeated_twice,
+                mode=conv.padding_mode
+            )
+        else:
+            in_spike = F.pad(in_spike, pad=(pL, pL))
+
+    if trace_pre is None:
+        trace_pre = torch.zeros_like(
+            in_spike, device=in_spike.device, dtype=in_spike.dtype
+        )
+
+    if trace_post is None:
+        trace_post = torch.zeros_like(
+            out_spike, device=in_spike.device, dtype=in_spike.dtype
+        )
+
+    trace_pre = trace_pre - trace_pre / tau_pre + in_spike
+    trace_post = trace_post - trace_post / tau_post + out_spike
+
+    delta_w = torch.zeros_like(conv.weight.data)
+    for l in range(conv.weight.shape[2]):
+        l_end = in_spike.shape[2] - conv.weight.shape[2] + 1 + l
+        pre_spike = in_spike[:, :, l:l_end:stride_l]  # shape = [batch_size, C_in, l_out]
+        post_spike = out_spike  # shape = [batch_size, C_out, l_out]
+        weight = conv.weight.data[:, :, l]   # shape = [batch_size_out, C_in]
+
+        tr_pre = trace_pre[:, :, l:l_end:stride_l]    # shape = [batch_size, C_in, l_out]
+        tr_post = trace_post    # shape = [batch_size, C_out, l_out]
+
+        delta_w_pre = - (f_pre(weight) *
+                        (tr_post.unsqueeze(2) * pre_spike.unsqueeze(1))
+                        .permute([1, 2, 0, 3]).sum(dim = [2, 3]))
+        delta_w_post = f_post(weight) * \
+                       (tr_pre.unsqueeze(1) * post_spike.unsqueeze(2))\
+                       .permute([1, 2, 0, 3]).sum(dim = [2, 3])
+        delta_w[:, :, l] += delta_w_pre + delta_w_post
+
+    return trace_pre, trace_post, delta_w
 
 def stdp_multi_step(
-    layer: Union[nn.Linear, nn.Conv2d], 
+    layer: Union[nn.Linear, nn.Conv1d, nn.Conv2d],
     in_spike: torch.Tensor, out_spike: torch.Tensor,
     trace_pre: Union[float, torch.Tensor, None],
     trace_post: Union[float, torch.Tensor, None], 
-    tau_pre: float, tau_post: float, w_min: float, w_max: float,
+    tau_pre: float, tau_post: float,
     f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x 
 ):
     weight = layer.weight.data
     delta_w = torch.zeros_like(weight)
     T = in_spike.shape[0]
-    stdp_single_step = (stdp_linear_single_step if isinstance(layer, nn.Linear)
-                        else stdp_conv2d_single_step)
+
+    if isinstance(layer, nn.Linear):
+        stdp_single_step = stdp_linear_single_step
+
+    elif isinstance(layer, nn.Conv1d):
+        stdp_single_step = stdp_conv1d_single_step
+
+    elif isinstance(layer, nn.Conv2d):
+        stdp_single_step = stdp_conv2d_single_step
+
 
     for t in range(T):
         trace_pre, trace_post, dw = stdp_single_step(
             layer, in_spike[t], out_spike[t], trace_pre, trace_post,
-            tau_pre, tau_post, w_min, w_max, f_pre, f_post
+            tau_pre, tau_post, f_pre, f_post
         )
         delta_w += dw
 
@@ -175,15 +243,12 @@ class STDPLearner(base.MemoryModule):
         self, step_mode: str,
         synapse: Union[nn.Conv2d, nn.Linear], sn: neuron.BaseNode,
         tau_pre: float, tau_post: float,
-        w_min: float, w_max: float,
         f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
     ):
         super().__init__()
         self.step_mode = step_mode
         self.tau_pre = tau_pre
         self.tau_post = tau_post
-        self.w_min = w_min
-        self.w_max = w_max
         self.f_pre = f_pre
         self.f_post = f_post
         self.synapse = synapse
@@ -211,15 +276,16 @@ class STDPLearner(base.MemoryModule):
         delta_w = None
 
         if self.step_mode == 's':
-            if isinstance(self.synapse, nn.Conv2d):
-                stdp_f = stdp_conv2d_single_step
-            elif isinstance(self.synapse, nn.Linear):
+            if isinstance(self.synapse, nn.Linear):
                 stdp_f = stdp_linear_single_step
+            elif isinstance(self.synapse, nn.Conv2d):
+                stdp_f = stdp_conv2d_single_step
+            elif isinstance(self.synapse, nn.Conv1d):
+                stdp_f = stdp_conv1d_single_step
             else:
                 raise NotImplementedError(self.synapse)
         elif self.step_mode == 'm':
-            if (isinstance(self.synapse, nn.Conv2d) or 
-                isinstance(self.synapse, nn.Linear)):
+            if isinstance(self.synapse, (nn.Linear, nn.Conv1d, nn.Conv2d)):
                 stdp_f = stdp_multi_step
             else:
                 raise NotImplementedError(self.synapse)
@@ -234,7 +300,6 @@ class STDPLearner(base.MemoryModule):
                 self.synapse, in_spike, out_spike,
                 self.trace_pre, self.trace_post, 
                 self.tau_pre, self.tau_post,
-                self.w_min, self.w_max,
                 self.f_pre, self.f_post
             )
             if scale != 1.:
@@ -256,7 +321,6 @@ class MSTDPLearner(base.MemoryModule):
         self, step_mode: str, batch_size: float,
         synapse: Union[nn.Conv2d, nn.Linear], sn: neuron.BaseNode,
         tau_pre: float, tau_post: float,
-        w_min: float, w_max: float,
         f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
     ):
         super().__init__()
@@ -264,8 +328,6 @@ class MSTDPLearner(base.MemoryModule):
         self.batch_size = batch_size
         self.tau_pre = tau_pre
         self.tau_post = tau_post
-        self.w_min = w_min
-        self.w_max = w_max
         self.f_pre = f_pre
         self.f_post = f_post
         self.synapse = synapse
@@ -330,7 +392,6 @@ class MSTDPLearner(base.MemoryModule):
                 self.synapse, in_spike, out_spike,
                 self.trace_pre, self.trace_post, 
                 self.tau_pre, self.tau_post,
-                self.w_min, self.w_max,
                 self.f_pre, self.f_post
             )
 
@@ -346,7 +407,7 @@ class MSTDPLearner(base.MemoryModule):
 class MSTDPETLearner(base.MemoryModule):
     def __init__(
         self, step_mode: str, synapse: Union[nn.Conv2d, nn.Linear], sn: neuron.BaseNode,
-        tau_pre: float, tau_post: float, tau_trace: float, w_min: float, w_max: float,
+        tau_pre: float, tau_post: float, tau_trace: float,
         f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x
     ):
         super().__init__()
@@ -354,8 +415,6 @@ class MSTDPETLearner(base.MemoryModule):
         self.tau_pre = tau_pre
         self.tau_post = tau_post
         self.tau_trace = tau_trace
-        self.w_min = w_min
-        self.w_max = w_max
         self.f_pre = f_pre
         self.f_post = f_post
         self.synapse = synapse
@@ -426,7 +485,7 @@ class MSTDPETLearner(base.MemoryModule):
                 self.synapse, in_spike, out_spike,
                 self.trace_pre, self.trace_post, 
                 self.tau_pre, self.tau_post, self.tau_trace,
-                self.w_min, self.w_max, self.f_pre, self.f_post
+                self.f_pre, self.f_post
             )
 
         if on_grad:
