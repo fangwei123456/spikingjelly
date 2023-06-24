d4aae33241492e47f3e734674c6aafbdae8f1897
AllenYolk <allen.yfhuang@gmail.com>
2023-06-24 13:42:01 +0800

    docs: add docs for Bullying10K dataset.

--- spikingjelly/datasets/bullying10k.py
+++ spikingjelly/datasets/bullying10k.py
@@ -33,7 +33,6 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
     def __init__(
         self, 
         root: str, 
-        pose_estimation: bool = False,
         train: Optional[bool] = None,
         data_type: str = 'event',
         frames_number: Optional[int] = None,
@@ -44,10 +43,13 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
     ) -> None:
-        if pose_estimation:
-            raise NotImplementedError(
-                "The pose estimation task has not been implemented yet!"
-            )
+        """
+        The Bullying10K dataset for action recognition (classification), which 
+        is proposed by `Bullying10K: A Neuromorphic Dataset towards 
+        Privacy-Preserving Bullying Recognition <https://arxiv.org/abs/2306.11546>`_.
+
+        Refer to :class:`spikingjelly.datasets.NeuromorphicDatasetFolder` for more details about params information.
+        """
         if train is None:
             raise ValueError(
                 "The argument `train` must be specified as a boolean value."
@@ -61,25 +63,42 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
 
     @staticmethod
     def resource_url_md5() -> List[Tuple[str, str, str]]:
+        '''
+        :return: A list ``url`` that ``url[i]`` is a tuple, which contains the i-th file's name, download link, and MD5
+        :rtype: list
+        '''
         return [
             ("handshake.zip", "https://figshare.com/ndownloader/files/41268834", "681d70f499e736a1e805305284ddc425"),
-            #("slapping.zip", "https://figshare.com/ndownloader/files/41247021", "84b41d6805958f9f62f425223916ffc2"),
-            #("punching.zip", "https://figshare.com/ndownloader/files/41263314", "40954f480ab210099d448b7b88fc4719"),
-            #("walking.zip", "https://figshare.com/ndownloader/files/41247024", "56e4cac9c0814ce701c3b2292c15b6a9"),
-            #("fingerguess.zip", "https://figshare.com/ndownloader/files/41253057", "f83114e5b4f0ea57cac86fb080c7e4d7"),
-            #("strangling.zip", "https://figshare.com/ndownloader/files/41261904", "8185ecd6f3147e9b609d22f06270aa86"),
-            #("greeting.zip", "https://figshare.com/ndownloader/files/41268792", "4a763fad728b04c8356db8544f1121fe"),
-            #("pushing.zip", "https://figshare.com/ndownloader/files/41268951", "7986c74ade7149a98672120a89b13ba8"),
-            #("hairgrabs.zip", "https://figshare.com/ndownloader/files/41277855", "a9cf690ed0a3305da4a4b8e110f64db1"),
-            #("kicking.zip", "https://figshare.com/ndownloader/files/41278008", "6c3218f977de4ac29c84a10b17779c33"),
+            ("slapping.zip", "https://figshare.com/ndownloader/files/41247021", "84b41d6805958f9f62f425223916ffc2"),
+            ("punching.zip", "https://figshare.com/ndownloader/files/41263314", "40954f480ab210099d448b7b88fc4719"),
+            ("walking.zip", "https://figshare.com/ndownloader/files/41247024", "56e4cac9c0814ce701c3b2292c15b6a9"),
+            ("fingerguess.zip", "https://figshare.com/ndownloader/files/41253057", "f83114e5b4f0ea57cac86fb080c7e4d7"),
+            ("strangling.zip", "https://figshare.com/ndownloader/files/41261904", "8185ecd6f3147e9b609d22f06270aa86"),
+            ("greeting.zip", "https://figshare.com/ndownloader/files/41268792", "4a763fad728b04c8356db8544f1121fe"),
+            ("pushing.zip", "https://figshare.com/ndownloader/files/41268951", "7986c74ade7149a98672120a89b13ba8"),
+            ("hairgrabs.zip", "https://figshare.com/ndownloader/files/41277855", "a9cf690ed0a3305da4a4b8e110f64db1"),
+            ("kicking.zip", "https://figshare.com/ndownloader/files/41278008", "6c3218f977de4ac29c84a10b17779c33"),
         ]
 
     @staticmethod
     def downloadable() -> bool:
+        '''
+        :return: Whether the dataset can be directly downloaded by python codes. If not, the user have to download it manually
+        :rtype: bool
+        '''
         return True
 
     @staticmethod
     def extract_downloaded_files(download_root: str, extract_root: str) -> None:
+        '''
+        :param download_root: Root directory path which saves downloaded dataset files
+        :type download_root: str
+        :param extract_root: Root directory path which saves extracted files from downloaded files
+        :type extract_root: str
+        :return: None
+
+        This function defines how to extract downloaded files.
+        '''
         with ThreadPoolExecutor(
             max_workers=min(multiprocessing.cpu_count(), 10)
         ) as tpe:
@@ -104,8 +123,21 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
 
     @staticmethod
     def convert_npy_to_npz(src_path: str, dst_dir: str, label: int):
+        """
+        :param src_path: the path of a npy file
+        :type src_path: str
+        :param dst_dir: the path of the directory to contain the converted npz files
+        :type dst_dir: str
+        :param label: the label of the sample, ranging from 0 to 9
+        :type label: int
+        :return: None
+        
+        This function defines how to convert a single npy file to npz format and save converted file in ``dst_dir``.
+        """
         original_data = np.load(src_path, allow_pickle=True)
         original_data = [y for x in original_data for y in x]
+        # original_data: [(t, x, y, p, ...), ...]
+        # npz data: {"t": t, "x": x, "y": y, "p": p, "label": label}
         t = np.array([d[0] for d in original_data])
         x = np.array([d[1] for d in original_data])
         y = np.array([d[2] for d in original_data])
@@ -121,6 +153,15 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
 
     @staticmethod
     def create_events_np_files(extract_root: str, events_np_root: str) -> None:
+        '''
+        :param extract_root: Root directory path which saves extracted files from downloaded files
+        :type extract_root: str
+        :param events_np_root: Root directory path which saves events files in the ``npz`` format
+        :type events_np_root: str
+        :return: None
+
+        This function defines how to convert the extracted npy data in ``extract_root`` to ``npz`` format and save converted files in ``events_np_root``.
+        '''
         train_dir= os.path.join(events_np_root, "train")
         val_dir = os.path.join(events_np_root, "val")
         os.mkdir(train_dir)
@@ -193,5 +234,10 @@ class Bullying10kClassification(sjds.NeuromorphicDatasetFolder):
 
     @staticmethod
     def get_H_W() -> Tuple:
+        '''
+        :return: A tuple ``(H, W)``, where ``H`` is the height of the data and ``W` is the weight of the data.
+            For example, this function returns ``(128, 128)`` for the DVS128 Gesture dataset.
+        :rtype: tuple
+        '''
         return 260, 346
 
