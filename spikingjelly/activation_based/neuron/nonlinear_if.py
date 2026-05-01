6173843863939ebbea846f8ae84a19e8112120bb
AllenYolk <allen.yfhuang@gmail.com>
2026-05-02 00:00:23 +0800

    fix: remove cupy -> torch fallback

--- spikingjelly/activation_based/neuron/nonlinear_if.py
+++ spikingjelly/activation_based/neuron/nonlinear_if.py
@@ -186,21 +186,18 @@ class QIFNode(BaseNode):
             return super().multi_step_forward(x_seq)
         elif self.backend == "cupy":
             self.v_float_to_tensor(x_seq[0])
-            try:
-                spike_seq, v_seq = cuda_kernel.multistep_qif_ptt(
-                    x_seq.flatten(1),
-                    self.v.flatten(0),
-                    self.tau,
-                    self.v_threshold,
-                    self.v_reset,
-                    self.v_rest,
-                    self.v_c,
-                    self.a0,
-                    self.detach_reset,
-                    self.surrogate_function,
-                )
-            except Exception:
-                return super().multi_step_forward(x_seq)
+            spike_seq, v_seq = cuda_kernel.multistep_qif_ptt(
+                x_seq.flatten(1),
+                self.v.flatten(0),
+                self.tau,
+                self.v_threshold,
+                self.v_reset,
+                self.v_rest,
+                self.v_c,
+                self.a0,
+                self.detach_reset,
+                self.surrogate_function,
+            )
 
             spike_seq = spike_seq.reshape(x_seq.shape)
             v_seq = v_seq.reshape(x_seq.shape)
@@ -397,21 +394,18 @@ class EIFNode(BaseNode):
             return super().multi_step_forward(x_seq)
         elif self.backend == "cupy":
             self.v_float_to_tensor(x_seq[0])
-            try:
-                spike_seq, v_seq = cuda_kernel.multistep_eif_ptt(
-                    x_seq.flatten(1),
-                    self.v.flatten(0),
-                    self.tau,
-                    self.v_threshold,
-                    self.v_reset,
-                    self.v_rest,
-                    self.theta_rh,
-                    self.delta_T,
-                    self.detach_reset,
-                    self.surrogate_function,
-                )
-            except Exception:
-                return super().multi_step_forward(x_seq)
+            spike_seq, v_seq = cuda_kernel.multistep_eif_ptt(
+                x_seq.flatten(1),
+                self.v.flatten(0),
+                self.tau,
+                self.v_threshold,
+                self.v_reset,
+                self.v_rest,
+                self.theta_rh,
+                self.delta_T,
+                self.detach_reset,
+                self.surrogate_function,
+            )
 
             spike_seq = spike_seq.reshape(x_seq.shape)
             v_seq = v_seq.reshape(x_seq.shape)
